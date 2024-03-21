aee6d49777ba1c7d4b8bfa3735c73cf7a717bf51
AaronJ909 <156019531+AaronJ909@users.noreply.github.com>
2024-03-21 18:54:35 -0400

    Added the event listener and partial function while also configuring the newStory, xItem, yItem and zItem values

--- Assignment 4/Part 1/script.js
+++ Assignment 4/Part 1/script.js
@@ -15,3 +15,29 @@ let insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 let insertY = ["the soup kitchen", "Disneyland", "the White House"];
 
 let insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+
+
+
+randomize.addEventListener('click', result);
+
+function result() {
+    let newStory = storyText;
+    let xItem = randomValueFromArray(insertX);
+    let yItem = randomValueFromArray(insertY);
+    let zItem = randomValueFromArray(insertZ);
+
+
+  if(customName.value !== '') {
+    const name = customName.value;
+
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round(300);
+    const temperature =  Math.round(94);
+
+  }
+
+  story.textContent = ;
+  story.style.visibility = 'visible';
+}
