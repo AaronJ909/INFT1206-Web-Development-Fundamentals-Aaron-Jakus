103dbc1cb0f80d6679a4e95063185df4b88f9b36
AaronJ909 <156019531+AaronJ909@users.noreply.github.com>
2024-03-21 21:36:52 -0400

    added alternative text for each image

--- Assignment 4/Part 2/main.js
+++ Assignment 4/Part 2/main.js
@@ -8,6 +8,13 @@ const overlay = document.querySelector('.overlay');
 const images = ["pic1", "pic2", "pic3", "pic4", "pic 5"];
 
 /* Declaring the alternative text for each image file */
+const alternativeText = {
+    "pic1.jpg": "pic1 alt text",
+    "pic2.jpg": "pic2 alt text",
+    "pic3.jpg": "pic3 alt text",
+    "pic4.jpg": "pic4 alt text",
+    "pic5.jpg": "pic5 alt text",
+};
 
 /* Looping through images */
 
