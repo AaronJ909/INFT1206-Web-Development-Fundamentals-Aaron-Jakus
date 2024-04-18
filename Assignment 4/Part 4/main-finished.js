02dba7d160f0cd31f661d094f0a0e930fad17909
AaronJ909 <156019531+AaronJ909@users.noreply.github.com>
2024-04-18 12:40:41 -0400

    fixed the update function within the ball method

--- Assignment 4/Part 4/main-finished.js
+++ Assignment 4/Part 4/main-finished.js
@@ -59,6 +59,28 @@ class Ball extends Shape {
     }
   }
 
+  update() {
+    if ((this.x + this.size) >= width) {
+      this.velX = -(this.velX);
+    }
+  
+    if ((this.x - this.size) <= 0) {
+      this.velX = -(this.velX);
+    }
+  
+    if ((this.y + this.size) >= height) {
+      this.velY = -(this.velY);
+    }
+  
+    if ((this.y - this.size) <= 0) {
+      this.velY = -(this.velY);
+    }
+  
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+  
+
   collisionDetect(balls) {
     for (let i = 0; i < balls.length; i++) {
       if (balls[i].exists) {
